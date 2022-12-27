e0993237f9e5f26a59f4fc462fea4e2f2f0b059e
neshure <jpdevopsvm@gmail.com>
2022-12-26 21:10:42 -0600

    updated portfolio

---
+++ myproject/portfolio/js/script.js
@@ -0,0 +1,7 @@
+/* ============================typing animation ============= */
+var typed = new Typed(".typing", {
+  strings:["","Web Developer","Devops Engineer"],
+  typeSpeed:80,
+  BackSpeed:60,
+  loop:true
+})
